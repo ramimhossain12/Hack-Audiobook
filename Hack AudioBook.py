8aff6eaafae5326ce37143ecb126a3d9afc0bd47
ramimhossain12 <ramimhossain6242@gmail.com>
2020-10-11 23:51:12 +0600

    first commit

---
+++ Hack AudioBook.py
@@ -0,0 +1,9 @@
+import pyttsx3
+speaker = pyttsx3.init()
+speaker.say(
+    "ou need to find a community to learn which guide you to success as a software engineer. Therefore, when you are surrounded by a community of like-minded people, you have an automatic support system. A real-world software engineering community gives you guidance on where to focus your learning. Sites like Meetup is an ideal choice to find other professionals."
+
+
+
+)
+speaker.runAndWait()
